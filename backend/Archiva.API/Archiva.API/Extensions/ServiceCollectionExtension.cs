d7588ceb7ec45f10cac4c01e3fb9db734a633f8f
Nikolay Mihalev <nikimihalev1213@gmail.com>
2025-01-30 11:12:32 +0200

    Added extensions for app services

---
+++ backend/Archiva.API/Archiva.API/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,22 @@
+﻿using Archiva.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection") ?? throw new ArgumentException("The connection string was not found!");
+
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services) 
+        {
+            return services;
+        }
+    }
+}
