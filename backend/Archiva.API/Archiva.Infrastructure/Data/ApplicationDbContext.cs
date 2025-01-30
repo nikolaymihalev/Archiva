224ee6cee6d8b8808fff5c4edba500a894579396
Nikolay Mihalev <nikimihalev1213@gmail.com>
2025-01-30 11:24:07 +0200

    Created User entity

--- backend/Archiva.API/Archiva.Infrastructure/Data/ApplicationDbContext.cs
+++ backend/Archiva.API/Archiva.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Archiva.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Archiva.Infrastructure.Data
 {
@@ -7,6 +8,8 @@ namespace Archiva.Infrastructure.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
         {            
         }
+
+        public DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
---
+++ backend/Archiva.API/Archiva.Infrastructure/Models/User.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Archiva.Infrastructure.Models
+{
+    /// <summary>
+    /// Represents the User entity
+    /// </summary>
+    [Comment("Represents the User")]
+    public class User
+    {
+        /// <summary>
+        /// Unique idenfifier of the user
+        /// </summary>
+        [Key]
+        [Comment("User identifier")]
+        public string Id { get; set; } = string.Empty;
+
+        /// <summary>
+        /// First name of the user
+        /// </summary>
+        [Required]
+        [Comment("User first name")]
+        public string FirstName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Last name of the user
+        /// </summary>
+        [Required]
+        [Comment("User last name")]
+        public string LastName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Email of the user
+        /// </summary>
+        [Required]
+        [Comment("User email")]
+        public string Email { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Password of the user
+        /// </summary>
+        [Required]
+        [Comment("User password")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
