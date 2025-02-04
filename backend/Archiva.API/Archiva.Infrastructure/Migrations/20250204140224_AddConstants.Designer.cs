b05740fc06d44bff28903dc5663a0336f3e55236
Nikolay Mihalev <nikimihalev1213@gmail.com>
2025-02-04 16:02:47 +0200

    Applied migrations

--- backend/Archiva.API/Archiva.Infrastructure/Migrations/20250204140224_AddConstants.Designer.cs
+++ backend/Archiva.API/Archiva.Infrastructure/Migrations/20250204140224_AddConstants.Designer.cs
@@ -4,6 +4,7 @@ using Archiva.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Archiva.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250204140224_AddConstants")]
+    partial class AddConstants
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/Archiva.API/Archiva.Infrastructure/Migrations/20250204140224_AddConstants.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Archiva.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddConstants : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Users",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                comment: "User last name",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldComment: "User last name");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Users",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                comment: "User first name",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldComment: "User first name");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Documents",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                comment: "Document name",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldComment: "Document name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                comment: "User last name",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldComment: "User last name");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                comment: "User first name",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldComment: "User first name");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Documents",
+                type: "nvarchar(max)",
+                nullable: false,
+                comment: "Document name",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)",
+                oldMaxLength: 150,
+                oldComment: "Document name");
+        }
+    }
+}
