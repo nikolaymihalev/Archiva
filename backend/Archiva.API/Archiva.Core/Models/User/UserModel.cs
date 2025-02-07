66578f821cab0370f59f16beb44d09c36c5541a3
Nikolay Mihalev <nikimihalev1213@gmail.com>
2025-02-07 18:15:16 +0200

    Created User model

---
+++ backend/Archiva.API/Archiva.Core/Models/User/UserModel.cs
@@ -0,0 +1,33 @@
+﻿namespace Archiva.Core.Models.User
+{
+    /// <summary>
+    /// Model for user information
+    /// </summary>
+    public class UserModel
+    {
+        /// <summary>
+        /// Unique idenfifier of the user
+        /// </summary>
+        public string Id { get; set; } = string.Empty;
+
+        /// <summary>
+        /// First name of the user
+        /// </summary>
+        public string FirstName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Last name of the user
+        /// </summary>
+        public string LastName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Email of the user
+        /// </summary>
+        public string Email { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Password of the user
+        /// </summary>
+        public string Password { get; set; } = string.Empty;
+    }
+}
