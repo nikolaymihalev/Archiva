ed8fa49a380bff575ec06594faf01033052bc22f
Nikolay Mihalev <nikimihalev1213@gmail.com>
2025-02-10 17:22:45 +0200

    Added methods in Document service

--- backend/Archiva.API/Archiva.Core/Contracts/IDocumentService.cs
+++ backend/Archiva.API/Archiva.Core/Contracts/IDocumentService.cs
@@ -1,6 +1,14 @@
-﻿namespace Archiva.Core.Contracts
+﻿using Archiva.Core.Models;
+using Archiva.Core.Models.Document;
+
+namespace Archiva.Core.Contracts
 {
     public interface IDocumentService
     {
+        Task<IEnumerable<DocumentInfoModel>> GetUserDocumentsAsync(string userId);
+        Task AddAsync(DocumentFormModel model);
+        Task EditAsync(DocumentFormModel model);
+        Task DeleteAsync(string userId, int id);
+        Task<DocumentInfoModel> GetByIdAsync(int id);
     }
 }
