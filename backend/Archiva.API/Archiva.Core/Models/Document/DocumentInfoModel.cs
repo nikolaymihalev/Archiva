24f9fdab8ebae1b3efe497509ffcbd1f5bcba90e
Nikolay Mihalev <nikimihalev1213@gmail.com>
2025-02-10 17:11:52 +0200

    Created document info

---
+++ backend/Archiva.API/Archiva.Core/Models/Document/DocumentInfoModel.cs
@@ -0,0 +1,38 @@
+﻿namespace Archiva.Core.Models.Document
+{
+    /// <summary>
+    /// Model for document information
+    /// </summary>
+    public class DocumentInfoModel
+    {
+        /// <summary>
+        /// Unique idenfifier of the document
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Name of the document
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Image of the document
+        /// </summary>
+        public byte[] Image { get; set; } = new byte[128];
+
+        /// <summary>
+        /// Description of the document
+        /// </summary>
+        public string Description { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Issue date of the document
+        /// </summary>
+        public DateTime IssueDate { get; set; }
+
+        /// <summary>
+        /// End date of the document
+        /// </summary>
+        public DateTime EndDate { get; set; }
+    }
+}
