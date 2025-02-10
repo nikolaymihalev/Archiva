4d606878ad1a8340cb816a01491b3547c5a62231
Nikolay Mihalev <nikimihalev1213@gmail.com>
2025-02-10 17:41:22 +0200

    Implemented methods logic in Document service

--- backend/Archiva.API/Archiva.Core/Services/DocumentService.cs
+++ backend/Archiva.API/Archiva.Core/Services/DocumentService.cs
@@ -1,8 +1,130 @@
-﻿using Archiva.Core.Contracts;
+﻿using Archiva.Core.Constants;
+using Archiva.Core.Contracts;
+using Archiva.Core.Models;
+using Archiva.Core.Models.Document;
+using Archiva.Infrastructure.Common;
+using Archiva.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+using Document = Archiva.Infrastructure.Models.Document;
 
 namespace Archiva.Core.Services
 {
     public class DocumentService : IDocumentService
     {
+        private readonly IRepository repository;
+
+        public DocumentService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+
+        public async Task AddAsync(DocumentFormModel model)
+        {
+            var document = new Document()
+            {
+                Name = model.Name,
+                Image = model.Image,
+                Description = model.Description,
+                IssueDate = model.IssueDate,
+                EndDate = model.EndDate,
+            };
+
+            try
+            {
+                await repository.AddAsync(document);
+                await repository.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                throw new ArgumentException(ReturnMessages.OperationFailed);
+            }
+        }
+
+        public async Task DeleteAsync(string userId, int id)
+        {
+            var document = await repository.GetByIdAsync<Document>(id);
+
+            if (document is null)
+                throw new ArgumentException(string.Format(ReturnMessages.DoesntExist, "Document"));
+
+            var user = await repository.GetByIdAsync<User>(userId);
+
+            if (user is null)
+                throw new ArgumentException(string.Format(ReturnMessages.DoesntExist, "User"));
+
+            repository.Delete(new UserDocument() { DocumentId = id, UserId = userId });
+
+            await repository.SaveChangesAsync();
+        }
+
+        public async Task EditAsync(DocumentFormModel model)
+        {
+            var document = await repository.GetByIdAsync<Document>(model.Id);
+
+            if (document is null)
+                throw new ArgumentException(string.Format(ReturnMessages.DoesntExist, "Document"));
+
+            document.Name = model.Name;
+            document.Description = model.Description;
+            document.Image = model.Image;
+            document.IssueDate = model.IssueDate;
+            document.EndDate = model.EndDate;
+
+            await repository.SaveChangesAsync();    
+        }
+
+        public async Task<DocumentInfoModel> GetByIdAsync(int id)
+        {
+            var document = await repository.GetByIdAsync<Document>(id);
+
+            if(document is null)
+                throw new ArgumentException(string.Format(ReturnMessages.DoesntExist, "Document"));
+
+            return new DocumentInfoModel()
+            {
+                Id = document.Id,
+                Name = document.Name,
+                Image = document.Image,
+                Description = document.Description,
+                IssueDate = document.IssueDate,
+                EndDate = document.EndDate,
+            };
+        }
+
+        public async Task<IEnumerable<DocumentInfoModel>> GetUserDocumentsAsync(string userId)
+        {
+            var user = await repository.GetByIdAsync<User>(userId);
+
+            if (user is null)
+                throw new ArgumentException(string.Format(ReturnMessages.DoesntExist, "User"));
+
+            var documents = new List<DocumentInfoModel>();
+
+            var userDocumentsIds = await repository.AllReadonly<UserDocument>()
+                .Where(x => x.UserId == userId)
+                .ToListAsync();
+
+            foreach(var item in userDocumentsIds)
+            {
+                var documentEn = await repository.GetByIdAsync<Document>(item.DocumentId);
+
+                if (documentEn is null)
+                    continue;
+
+                var document = new DocumentInfoModel()
+                {
+                    Id = documentEn.Id,
+                    Name = documentEn.Name,
+                    Image = documentEn.Image,
+                    Description = documentEn.Description,
+                    IssueDate = documentEn.IssueDate,
+                    EndDate = documentEn.EndDate,
+                };
+
+                documents.Add(document);
+            }
+
+            return documents;
+        }
     }
 }
